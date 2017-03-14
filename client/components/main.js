3a02e8070e38045160357b6f4105e8d8596a31b9
Mupat <dev@mupat.net>
2017-03-14 16:31:23 +0100

    add leaflet and basic map

--- client/components/main.js
+++ client/components/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
-// import Insurance from './insurance';
-// import Category from './category';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import L from 'leaflet'
 
 class Travap extends React.Component {
   constructor(props) {
@@ -9,36 +9,34 @@ class Travap extends React.Component {
       places: []
     };
 
-    // this.selectInsurance = this.selectInsurance.bind(this);
-    // this.selectCategory = this.selectCategory.bind(this);
-
     this.props.fetch.places().then(response => {
       this.setState({ places: response });
     });
   }
 
-  // selectInsurance(event) {
-  //   const value = event.target.value;
-  //   this.setState({ selectedInsurance: value });
-  //   this.props.fetch.categories(value).then(response => {
-  //     this.setState({ categories: JSON.parse(response).data });
-  //   });
-  // }
-  //
-  // selectCategory(event) {
-  //   const value = event.target.value;
-  //   this.setState({ selectedCategory: value });
-  // }
+  travapIcon(place) {
+    return L.divIcon({ className: 'travap-icon', html: place.name })
+  }
 
   render() {
     return (
       <div>
         <h1>Travap</h1>
-        <ul>
+        <Map center={[52.52, 13.40]} zoom={6} style={{height: '250px'}}>
+          <TileLayer
+            url='http://a.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg'
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            minZoom={3}
+            maxZoom={12}
+          />
           {this.state.places.map(place =>
-            <li key={ place.id }>{ place.name }</li>
+            <Marker key={place.id} position={[place.coordinates.lat, place.coordinates.lng]} icon={this.travapIcon(place)}>
+              <Popup>
+                <span>{place.name}</span>
+              </Popup>
+            </Marker>
           )}
-        </ul>
+        </Map>
       </div>
     );
   }
